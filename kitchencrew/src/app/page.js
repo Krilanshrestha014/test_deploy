427726f8aea805d25dd5587d0dd80082bf0e1776
/* HTML: div class=loader/div */ <krilanshrestha@gmail.com>
2025-02-06 16:04:47 +0545

    first commit

---
+++ kitchencrew/src/app/page.js
@@ -0,0 +1,26 @@
+// function loop(){
+// for (let i=20; i>0; i--){
+//     console.log(i);
+// }}
+// document.getElementById("btn").addEventListener("click", loop)
+
+
+
+
+
+
+
+
+
+
+
+function even(){
+for (let i=1; i<=100; i++){if (i=i%2){
+    console.log(i)
+}
+
+    
+}
+
+}
+even()
